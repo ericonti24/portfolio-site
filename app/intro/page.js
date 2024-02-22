9c91c202bcc59b177161b8f4774bad523b79aa2c
Eric <ericontiveros123@gmail.com>
2024-02-21 18:37:46 -0700

    fix intro

--- app/intro/page.js
+++ app/intro/page.js
@@ -1,23 +1,30 @@
 "use client"
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Box,
   Container,
   Text,
   Flex,
   Heading,
-  useColorModeValue,
-  useBreakpointValue,
   Grid,
   Image
 } from '@chakra-ui/react';
-// import selfImage from '../api/img_bnr/self-image3.png'
-
-// console.log(selfImage);
 
 const IntroSection = () => {
-//   const isMobile = useBreakpointValue({ base: true, md: false });
+  const [isMobile, setIsMobile] = useState(false);
+  const selfImage = "/_next/static/media/self-image3.f98e0bfe.png"
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); 
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <Container maxWidth="7xl" p={{ base: 2, sm: 10 }} >
@@ -37,21 +44,23 @@ const IntroSection = () => {
             I enjoy web designing, developing and passionate about tech!
           </Text>
         </Box>
-        {/* {!isMobile && (
+        {!isMobile && (
           <Box>
-            <Image src={"/_next/static/media/self-image3.f98e0bfe.png"} alt="Eric's Image" borderRadius="15px 90px" boxSize="500px" />
+            <Image src={selfImage} alt="Eric's Image" borderRadius="15px 90px" boxSize="500px" />
           </Box>
-        )} */}
+        )}
       </Grid>
-      {/* {isMobile && (
+      {isMobile && (
         <Flex justifyContent="center" mt={8} mb={20}>
           <Box>
-            <Image src={"/_next/static/media/self-image3.f98e0bfe.png"} alt="Eric's Image" borderRadius="15px 90px" boxSize="300px" />
+            <Image src={selfImage} alt="Eric's Image" borderRadius="15px 90px" boxSize="300px" />
           </Box>
         </Flex>
-      )} */}
+      )}
     </Container>
   );
 };
+
 export default IntroSection;
+
 
