e1cc4091e1d8f5f9b3745a795d237dfdad9b4c90
Eric <ericontiveros123@gmail.com>
2024-02-22 09:18:46 -0700

    reomved useBreakpointValue from milestones

--- app/milestones/page.js
+++ app/milestones/page.js
@@ -1,5 +1,156 @@
 'use client'
-import React from 'react';
+// import React from 'react';
+// import {
+//   Box,
+//   chakra,
+//   Container,
+//   Text,
+//   HStack,
+//   VStack,
+//   Flex,
+//   useColorModeValue,
+//   useBreakpointValue
+// } from '@chakra-ui/react';
+// import { MILESTONES } from '../api/milestones/milestonesData';
+
+// const Milestones = () => {
+//   const isMobile = useBreakpointValue({ base: true, md: false});
+//   const isDesktop = useBreakpointValue({ base: false, md: true});
+
+//   return (
+//     <Container id='milestones-section' maxWidth="7xl" p={{ base: 2, sm: 10 }}>
+//       <chakra.h3 fontSize="4xl" fontWeight="bold" mb={4} textAlign="center" color={'white'}>
+//         MILESTONES
+//       </chakra.h3>
+//       {MILESTONES.map((milestone) => (
+//         <Flex key={milestone.id} mb="10px">
+//           {/* Desktop view(left card) */}
+//           {isDesktop && milestone.id % 2 === 0 && (
+//             <>
+//               <EmptyCard />
+//               <LineWithDot />
+//               <Card {...milestone} />
+//             </>
+//           )}
+//           {/* Mobile view */}
+//           {isMobile && (
+//             <>
+//               <LineWithDot />
+//               <Card {...milestone} />
+//             </>
+//           )}
+//           {/* Desktop view(right card) */}
+//           {isDesktop && milestone.id % 2 !== 0 && (
+//             <>
+//               <Card {...milestone} />
+
+//               <LineWithDot />
+//               <EmptyCard />
+//             </>
+//           )}
+//         </Flex>
+//       ))}
+//     </Container>
+//   );
+// };
+
+// const Card = ({ id, title, description, date }) => {
+//   const isEvenId = id % 2 == 0;
+//   let borderWidthValue = isEvenId ? '15px 15px 15px 0' : '15px 0 15px 15px';
+//   let leftValue = isEvenId ? '-15px' : 'unset';
+//   let rightValue = isEvenId ? 'unset' : '-15px';
+
+//   const isMobile = useBreakpointValue({ base: true, md: false });
+//   if (isMobile) {
+//     leftValue = '-15px';
+//     rightValue = 'unset';
+//     borderWidthValue = '15px 15px 15px 0';
+//   }
+
+//   return (
+//     <HStack
+//       flex={1}
+//       p={{ base: 3, sm: 6 }}
+//     //   bg={useColorModeValue('gray', 'gray.800')}
+//      bg='white'
+//       spacing={5}
+//       rounded="lg"
+//       alignItems="center"
+//       pos="relative"
+//       _before={{
+//         content: `""`,
+//         w: '0',
+//         h: '0',
+//         borderColor: `transparent ${useColorModeValue('#edf2f6', '#1a202c')} transparent`,
+//         borderStyle: 'solid',
+//         borderWidth: borderWidthValue,
+//         position: 'absolute',
+//         left: leftValue,
+//         right: rightValue,
+//         display: 'block'
+//       }}
+//     >
+//       <Box>
+//         <Text fontSize="lg" color={isEvenId ? 'teal' : 'black'}>
+//           {date}
+//         </Text>
+
+//         <VStack spacing={2} mb={3} textAlign="left">
+//           <chakra.h1 fontSize="2xl" lineHeight={1.2} fontWeight="bold" w="100%">
+//             {title}
+//           </chakra.h1>
+//           <Text fontSize="md">{description}</Text>
+//         </VStack>
+//       </Box>
+//     </HStack>
+//   );
+// };
+
+// const LineWithDot = () => {
+//   return (
+//     <Flex
+//       pos="relative"
+//       alignItems="center"
+//       mr={{ base: '40px', md: '40px' }}
+//       ml={{ base: '0', md: '40px' }}
+//     >
+//       <chakra.span
+//         position="absolute"
+//         left="50%"
+//         height="calc(100% + 10px)"
+//         border="1px solid"
+//         borderColor={useColorModeValue('gray.200', 'gray.700')}
+//         top="0px"
+//       ></chakra.span>
+//       <Box pos="relative" p="10px">
+//         <Box
+//           pos="absolute"
+//           top="0"
+//           left="0"
+//           bottom="0"
+//           right="0"
+//           width="100%"
+//           height="100%"
+//           backgroundSize="cover"
+//           backgroundRepeat="no-repeat"
+//           backgroundPosition="center center"
+//           bg={useColorModeValue('teal')}
+//           borderRadius="100px"
+//           backgroundImage="none"
+//           opacity={1}
+//         ></Box>
+//       </Box>
+//     </Flex>
+//   );
+// };
+
+// const EmptyCard = () => {
+//   return <Box flex={{ base: 0, md: 1 }} p={{ base: 0, md: 6 }} bg="transparent"></Box>;
+// };
+
+// export default Milestones;
+
+import React, { useState, useEffect } from 'react';
 import {
   Box,
   chakra,
@@ -9,13 +160,23 @@ import {
   VStack,
   Flex,
   useColorModeValue,
-  useBreakpointValue
 } from '@chakra-ui/react';
 import { MILESTONES } from '../api/milestones/milestonesData';
 
 const Milestones = () => {
-  const isMobile = useBreakpointValue({ base: true, md: false});
-  const isDesktop = useBreakpointValue({ base: false, md: true});
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth < 768); // Adjust the width according to your breakpoint
+    }
+
+    handleResize(); // Call once to set the initial value
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <Container id='milestones-section' maxWidth="7xl" p={{ base: 2, sm: 10 }}>
@@ -25,7 +186,7 @@ const Milestones = () => {
       {MILESTONES.map((milestone) => (
         <Flex key={milestone.id} mb="10px">
           {/* Desktop view(left card) */}
-          {isDesktop && milestone.id % 2 === 0 && (
+          {!isMobile && milestone.id % 2 === 0 && (
             <>
               <EmptyCard />
               <LineWithDot />
@@ -40,10 +201,9 @@ const Milestones = () => {
             </>
           )}
           {/* Desktop view(right card) */}
-          {isDesktop && milestone.id % 2 !== 0 && (
+          {!isMobile && milestone.id % 2 !== 0 && (
             <>
               <Card {...milestone} />
-
               <LineWithDot />
               <EmptyCard />
             </>
@@ -54,13 +214,12 @@ const Milestones = () => {
   );
 };
 
-const Card = ({ id, title, description, date }) => {
-  const isEvenId = id % 2 == 0;
+const Card = ({ id, title, description, date, isMobile }) => {
+  const isEvenId = id % 2 === 0;
   let borderWidthValue = isEvenId ? '15px 15px 15px 0' : '15px 0 15px 15px';
   let leftValue = isEvenId ? '-15px' : 'unset';
   let rightValue = isEvenId ? 'unset' : '-15px';
 
-  const isMobile = useBreakpointValue({ base: true, md: false });
   if (isMobile) {
     leftValue = '-15px';
     rightValue = 'unset';
@@ -71,8 +230,7 @@ const Card = ({ id, title, description, date }) => {
     <HStack
       flex={1}
       p={{ base: 3, sm: 6 }}
-    //   bg={useColorModeValue('gray', 'gray.800')}
-     bg='white'
+      bg='white'
       spacing={5}
       rounded="lg"
       alignItems="center"
@@ -149,6 +307,7 @@ const EmptyCard = () => {
 };
 
 export default Milestones;
+
 
 
 
